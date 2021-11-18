e0b54e90b5e349a74802bc01fd8f21f994e27e0b
Michael Knutson <mrknutson44@gmail.com>
2021-11-18 12:12:33 -0900

    set up items model and created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "departments#index"
-  # (C)
-  get "/departments/new", to: "departments#new"
-  post "/departments", to: "departments#create"
+  # # (C)
+  # get "/departments/new", to: "departments#new"
+  # post "/departments", to: "departments#create"
 
-  # (R)
-  get "/departments/", to: "departments#index"
-  get "/departments/:id", to: "departments#show"
+  # # (R)
+  # get "/departments/", to: "departments#index"
+  # get "/departments/:id", to: "departments#show"
 
-  # (U)
-  get "departments/:id/edit", to: "departments#edit"
-  put "departments/:id", to: "departments#update"
-  patch "departments/:id", to: "departments#update"
-  # (D)
-  delete "departments/:id", to: "departments#destroy"
+  # # (U)
+  # get "departments/:id/edit", to: "departments#edit"
+  # put "departments/:id", to: "departments#update"
+  # patch "departments/:id", to: "departments#update"
+  # # (D)
+  # delete "departments/:id", to: "departments#destroy"
+
+  resources: departments do
+    resources: items
+  end
 end
